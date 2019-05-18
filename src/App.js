dfd7f539d5c5d9bbb85a22478371015478ae7deb
sumanthsm <sssumant444@gmail.com>
2019-05-19 04:25:30 +0530

    added homepage layout

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import './App.css';
 import CreateInvoice from './components/CreateInvoice';
 import InvoiceProcessing from './components/InvoiceProcessing';
 import InvoiceList from './components/InvoiceList';
+import HomePage from './components/HomePage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Router history={createBrowserHistory()}>
-          <Route path="/" exact={true} component={CreateInvoice} />
+          <Route path="/" exact={true} component={HomePage} />
           <Route path="/createinvoice" component={CreateInvoice} />
           <Route path="/processinvoice" component={InvoiceProcessing} />
           <Route path="/invoicelist" component={InvoiceList} />
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import InvoiceImage from '../invoice-image.jpg';
+import InvoiceImage from '../invoice-image.png';
 
 export default class HomePage extends React.Component {
     constructor(props) {
@@ -11,20 +11,22 @@ export default class HomePage extends React.Component {
     render() {
         return (
             <div>
+                <nav className="navbar navbar-expand-sm bg-primary navbar-dark">
+                            <span style={{ color: 'white', padding: '12px 0px' }}>Invoice management system</span>
+                        </nav>
                 <div className="container-fluid">
-                    <div className="card" style={{ margin: '50px auto' }}>
-                        <div className="card-body">
-                            <div className="row" style={{ margin: '0 auto', textAlign : 'center'}}>
-                                <div className="card" style={{width:"400px"}}>
-                                    <img className="card-img-top" src={InvoiceImage} alt="Card image" style={{width:"100%"}} />
+                    <div style={{margin: '20px auto'}}>
+                    <div className="row" style={{ margin: '0 auto', textAlign : 'center'}}>
+                                <div className="card" style={{width:"300px", margin: '20px', borderRadius: '20px'}}>
+                                    <img className="card-img-top" src={InvoiceImage} alt="Card image" style={{width:"50%", margin: '10px auto'}} />
                                     <div className="card-body">
                                         <h4 className="card-title">Create Invoice</h4>
                                         <p className="card-text">Click here to Create a New Invoice</p>
                                         <Link to="/createinvoice" className="btn btn-primary">Create Invoice</Link>
                                     </div>
                                 </div>
-                                <div className="card" style={{width:"400px"}}>
-                                    <img className="card-img-top" src={InvoiceImage} alt="Card image" style={{width:"100%"}}/>
+                                <div className="card" style={{width:"300px", margin: '20px', borderRadius: '20px'}}>
+                                    <img className="card-img-top" src={InvoiceImage} alt="Card image" style={{width:"50%", margin: '10px auto'}}/>
                                     <div className="card-body">
                                         <h4 className="card-title">Invoice List</h4>
                                         <p className="card-text">Click here to View all the Invoices</p>
@@ -32,7 +34,6 @@ export default class HomePage extends React.Component {
                                     </div>
                                 </div>
                             </div>
-                        </div>
                     </div>
                 </div>
             </div>
